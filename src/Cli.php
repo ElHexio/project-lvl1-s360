30fd3cf87c0248be62ab21800e3a94bbfa8d31fb
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2018-10-17 18:55:30 +0400

    composer autoloading files actualized

--- src/Cli.php
+++ src/Cli.php
@@ -7,4 +7,7 @@ use function BrainGames\ConsoleGame\play;
 function run()
 {
     line('Welcome to the Brain Games!');
+    $playerName = prompt('May I have your name?');
+    line("Hello, %s!\n", $playerName);
+    line('Congratulations, %s!', $playerName);
 }
