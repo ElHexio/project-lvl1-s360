720dfff154ad2687b8af9732845731aae10d17c0
Alexander Shcherbachenko <ascherbachenko@cs-cart.com>
2018-10-17 18:57:03 +0400

    use function statemet added to dummy Cli engine

--- src/Cli.php
+++ src/Cli.php
@@ -2,12 +2,12 @@
 
 namespace BrainGames\Cli;
 
-use function BrainGames\ConsoleGame\play;
+use function cli\line;
+use function cli\prompt;
 
 function run()
 {
     line('Welcome to the Brain Games!');
     $playerName = prompt('May I have your name?');
-    line("Hello, %s!\n", $playerName);
-    line('Congratulations, %s!', $playerName);
+    line("Hello, %s!", $playerName);
 }
